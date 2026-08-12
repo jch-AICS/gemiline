0805b492dc8783aaad867d97631f6ad339419f07
jch-AICS <316062804+jch-AICS@users.noreply.github.com>
2026-08-12 15:47:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def handle_message(event):
         try:
             user_id = event.source.user_id
             chat = chat_sessions.get(user_id) or client.chats.create(
-                model="Gemini-2.5-flash",
+                model="gemini-2.5-flash",
                 config=generation_config
             )
             chat_sessions[user_id] = chat
